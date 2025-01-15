3b19da43aa3743ac5ba7bde70de45c5496caaf9c
pedrofernandez <pedrofernandezgarmendia@gmail.com>
2025-01-15 18:52:41 +0100

    first file

--- secondfile.py
+++ secondfile.py
@@ -1,1 +1,23 @@
 print("new change")
+print(2+6)
+print(9-99)
+print(3*8)
+print(20/5) #always produces float result
+print(22/5)
+print(20//5) #this is the integer division
+print(22%5) # two extra numbers
+print(10%6)
+print(9%8)
+print(2**3)
+print(not True)
+print(not False)
+print(True and False)
+print(True or False)
+print(2 or 3 or 4) #always first one for or
+print(False or 0 or 4 or True)
+print(not 7) #False because 7 is true
+print(1 and 2 and 3) #always last one for and
+print(1 and 0.0 and 4) #the 0.0 is not true
+print(False or 0 or None) #all are false and it chooses none because it's the last one that could've saved it
+print()
+
